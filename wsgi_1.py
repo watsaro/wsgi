7b1224a4169991d7bf2359c0c451866aa48ba8c3
Aron Watson <watsaro@gmail.com>
2020-04-28 14:18:57 -0700

    Activity Lesson 04

--- wsgi_1.py
+++ wsgi_1.py
@@ -21,11 +21,11 @@ def application(environ, start_response):
 
     response_body = body.format(
         software=environ.get('SERVER_SOFTWARE', default),
-        path="aaaa",
-        month="bbbb",
-        date="cccc",
-        year="dddd",
-        client_ip="eeee"
+        path=environ.get('PATH_INFO', default),
+        month=datetime.datetime.now().month,
+        date=datetime.datetime.now().date,
+        year=datetime.datetime.now().year,
+        client_ip=environ.get('REMOTE_ADDR', default)
     )
     status = '200 OK'
 
